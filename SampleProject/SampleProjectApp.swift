d5808d4fb2019cef2847ed8608eb13d93651c9f0
ashoks-1404 <ashoks@alohatechnology.com>
2025-07-06 14:13:20 +0530

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by Ashok Singh on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
